ac204f21aaa1a2501f3a3dbfcc45b520ca0b1e0b
Nieman <s-niemanj@bsd405.org>
2024-02-05 12:43:16 -0800

    Trying to add a textbox

--- docs/js/test.js
+++ docs/js/test.js
@@ -1,2 +1,8 @@
-let d = new Date();
-document.dateLister.innerHTML = "<h1>Today's date is " + d + "</h1>"
+const textbox = document.getElementById("textbox");
+const log = document.getElementById("chatLog");
+
+textbox.addEventListener("input", sendMessage);
+
+function sendMessage(message) {
+    log.textContent = "\n"+message.target.value;
+}

77d5e4ed43bac15b2a53d798f18414ef254cef50
Nieman <s-niemanj@bsd405.org>
2024-02-06 08:37:21 -0800

    Began work on usernames, added button to send message without enter

--- docs/js/test.js
+++ docs/js/test.js
@@ -1,18 +1,38 @@
-const input = document.querySelector("input");
+const input = document.getElementById("chatInput");
 const log = document.getElementById("values");
+let connected = true;
+let typingUsername = "";
 let message = "";
+let username = "<User>";
 
 //input.addEventListener("focusin", () => {typing = true});
 input.addEventListener("input", updateValue);
-input.addEventListener("keyup", sendMessage);
+input.addEventListener("keyup", updateValue);
 
 function updateValue(e) {
+	if (e.key=="Enter") {
+        sendMessage(message);
+		input.value = "";
+    }
     message = e.target.value;
 }
 
-function sendMessage(e) {
-    if (e.key=="Enter") {
-        log.textContent += message+"\n";
-        input.value = "";
+function sendMessage(msg) {
+    log.textContent += username+" "+msg+"\n";
+}
+
+function recieveMessage(msg) {
+	log.textContent += msg+"\n";
+}
+
+function updateUser(e) {
+	if (e.key=="Enter") {
+        setUser();
     }
+    message = e.target.value;
+}
+
+function setUser() {
+	username = "<"+">";
+	connected = true;
 }

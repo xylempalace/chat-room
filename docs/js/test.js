4712e21021716836415b0d5b8d4c0b1a812df923
Nieman <s-niemanj@bsd405.org>
2024-02-08 15:19:31 -0800

    Began work on adding canvas

--- docs/js/test.js
+++ docs/js/test.js
@@ -3,6 +3,7 @@ const chatSend = document.getElementById("chatSend");
 const usernameInput = document.getElementById("usernameInput");
 const usernameSend = document.getElementById("usernameSend");
 const log = document.getElementById("values");
+let gameCanvas;
 let connected = false;
 let typingUsername = "";
 let message = "";
@@ -18,6 +19,28 @@ setDisabled(chatInput, !connected);
 setDisabled(chatSend, !connected);
 setDisabled(usernameInput, connected);
 setDisabled(usernameSend, connected);
+
+class Player {
+
+	static playerSizeX = 100;
+	static playerSizeY = 100;
+
+	constructor(posX, posY) {
+		this.posX = posX;
+		this.posY = posY;
+	}
+
+	drawText(cnv, message) {
+		var ctx = cnv.getContext("2d");
+		ctx.font = "30px Arial";
+		ctx.fillText(message, posX, posY+10);
+	}
+
+	drawPlayer(cnv) {
+		var ctx = cnv.getContext("2d");
+		ctx.fillRect(posX, posY, playerSizeX, playerSizeY);
+	}
+}
 
 document.addEventListener("readystatechange", (e) => {
 	if (e.target.readyState === "complete") {
@@ -68,6 +91,11 @@ function setUser() {
 		setDisabled(chatSend, !connected);
 		setDisabled(usernameInput, connected);
 		setDisabled(usernameSend, connected);
+		addCanvas();
+
+		const testPlayer = Player(200, 200);
+		testPlayer.drawPlayer(canvas);
+		testPlayer.drawText(canvas, "Hello");
 	}
 }
 
@@ -78,3 +106,24 @@ function setDisabled(element, state) {
 		element.removeAttribute("disabled");
 	}
 }
+
+function addCanvas() {
+	var canvas = document.createElement('canvas');
+	canvas.id     = "gameCanvas";
+	canvas.width  = document.getElementById('gameSpace').clientWidth*0.8;
+	canvas.height = document.getElementById('gameSpace').clientHeight;
+	canvas.style.zIndex = 8;
+	canvas.style.position = "absolute";
+	canvas.style.border = "1px solid";
+	canvas.style.borderColor = "black";
+	gameCanvas = canvas;
+	document.getElementById("gameSpace").appendChild(canvas);
+}
+
+function addSquare(x, y, w, h) {
+	var c = document.getElementById("gameCanvas");
+	var ctx = c.getContext("2d");
+	ctx.font = "30px Arial";
+	ctx.fillText("Hello World", 10, 50);
+}
+

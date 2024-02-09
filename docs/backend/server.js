7d7e708c2b1a722a3060bd4428c3eac7f2b05a6e
nikolai <s-repinn@bsd405.org>
2024-02-09 13:10:24 -0800

    Some notes

--- docs/backend/server.js
+++ docs/backend/server.js
@@ -1,11 +1,16 @@
+
+//Script used to send index.html in response to HTTP request
+
+//Import express library and declare it as var app
 const express = require('express')
 const app = express()
+
 const port = 3000
+//Import path library
 const path = require('path')
 
 app.get('/', (req, res) => {
   app.use('/static', express.static(path.join(__dirname, 'frontend')));
-  console.log(path.join(__dirname, 'frontend'));
   res.set('Content-Type', 'text/html');
   res.sendFile(path.join(__dirname, '../frontend/index.html'));
 })

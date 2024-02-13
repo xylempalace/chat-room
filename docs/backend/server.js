5304fd5b0a9032412bef5d2d39ddc055521b9f3d
nikolai <s-repinn@bsd405.org>
2024-02-13 13:10:12 -0800

    removed useless lines

--- docs/backend/server.js
+++ docs/backend/server.js
@@ -11,19 +11,16 @@ const path = require('path')
 
 //Sends index.html and coressponding css file, TODO: Send JS file as well.
 app.get('/', (req, res) => {
-  app.use(express.static("frontend"));
   res.set('Content-Type', 'text/html');
   res.sendFile(path.join(__dirname, '../frontend/index.html'));
 })
 
 app.get('/web.css', (req, res) => {
-  app.use(express.static("frontend"));
   res.set('Content-Type', 'text/css');
   res.sendFile(path.join(__dirname, '../frontend/web.css'));
 })
 
 app.get('/test.js', (req, res) => {
-  app.use(express.static("frontend"));
   res.set('Content-Type', 'text/javascript');
   res.sendFile(path.join(__dirname, '../frontend/test.js'));
 })

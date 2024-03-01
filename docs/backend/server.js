dd3a65515036caf9b90894a71e791e1ca1995762
nikolai <s-repinn@bsd405.org>
2024-03-01 13:36:56 -0800

    func spec

--- docs/backend/server.js
+++ docs/backend/server.js
@@ -33,66 +33,70 @@ var clients = {};
 app.get('/', (req, res) => {
   res.set('Content-Type', 'text/html');
   res.sendFile(path.join(__dirname, '../frontend/index.html'));
-})
+});
+
+app.get('/.', (req, res) => {
+  console.log(req);
+});
 
 app.get('/web.css', (req, res) => {
   res.set('Content-Type', 'text/css');
   res.sendFile(path.join(__dirname, '../frontend/web.css'));
-})
+});
 
 app.get('/test.js', (req, res) => {
  res.set('Content-Type', 'text/javascript');
   res.sendFile(path.join(__dirname, '../frontend/test.js'));
-})
+});
 
 app.get('/favicon.ico', (req, res) => {
   res.set('Content-Type', '	image/vnd.microsoft.icon');
   res.sendFile(path.join(__dirname, '../images/favicon.ico'));
-})
+});
 
 app.get('/lemEngine.js', (req, res) => {
   res.set('Content-Type', 'text/javascript');
    res.sendFile(path.join(__dirname, '../frontend/lemEngine.js'));
-})
+});
 
 app.get('/sprites/tiles/floor.png', (req, res) => {
   res.set('Content-Type', 'image/png');
    res.sendFile(path.join(__dirname, '../frontend/sprites/tiles/floor.png'));
-})
+});
 
 app.get('/sprites/tiles/wall.png', (req, res) => {
   res.set('Content-Type', 'image/png');
    res.sendFile(path.join(__dirname, '../frontend/sprites/tiles/wall.png'));
-})
+});
 
 app.get('/sprites/tiles/grass.png', (req, res) => {
   res.set('Content-Type', 'image/png');
    res.sendFile(path.join(__dirname, '../frontend/sprites/tiles/grass.png'));
-})
+});
 
 app.get('/sprites/tiles/pathCenter.png', (req, res) => {
   res.set('Content-Type', 'image/png');
    res.sendFile(path.join(__dirname, '../frontend/sprites/tiles/pathCenter.png'));
-})
+});
 
 app.get('/sprites/tiles/pathNorth.png', (req, res) => {
   res.set('Content-Type', 'image/png');
    res.sendFile(path.join(__dirname, '../frontend/sprites/tiles/pathNorth.png'));
-})
+});
 
 app.get('/sprites/tiles/pathSouth.png', (req, res) => {
   res.set('Content-Type', 'image/png');
    res.sendFile(path.join(__dirname, '../frontend/sprites/tiles/pathSouth.png'));
-})
+});
 
 app.get('/sprites/speechBubble.png', (req, res) => {
   res.set('Content-Type', 'image/png');
    res.sendFile(path.join(__dirname, '../frontend/sprites/speechBubble.png'));
-})
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
-})
+});
 
 sockserver.on('connection', ws => {
   console.log('New client connected!'); 

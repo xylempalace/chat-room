abffc93455ebfc51cc392df934a5de1aec25d52e
nikolai <s-repinn@bsd405.org>
2024-03-12 13:10:26 -0700

    Methods and tests for board class

--- docs/frontend/games.js
+++ docs/frontend/games.js
@@ -17,10 +17,80 @@ class Board {
         }
     }
 
-    set(pos, obj, condition) {
-        if (condition()) {
-            this.board[pos.x][pos.y] = obj;
+    get(pos) {
+        return this.board[pos.y][pos.x];
+    }
+
+    getRow(pos, right) {
+        if (right) {
+            return this.board[pos.y].slice(pos.x, this.board[pos.y].length);
+        } else {
+            return this.board[pos.y].slice(0, pos.x + 1).reverse();
         }
-        console.log(this.board);
+    }
+
+    getColumn(pos, down) {
+        var result = [];
+        for (var i = 0; i < this.rows; i++) {
+            result.push(this.board[i][pos.x]);
+        }
+        if (down) {
+            return result.slice(pos.y, result.length);
+        } else {
+            return result.slice(0, pos.y + 1).reverse();
+        }
+    }
+
+    getDiagonal(pos, x, y) {
+        var result = [];
+        var j = pos.x;
+        for (var i = pos.y; i < this.rows && i >= 0; i += y) {
+            if (j < this.columns && j >= 0) {
+                result.push(this.get(new Vector2(j, i)));
+            }
+            j += x;
+        }
+        return result;
+    }
+
+    set(pos, obj) {
+        this.board[pos.y][pos.x] = obj;
+    }
+
+    setPos(pos, obj, condition) {
+        if (condition(this.get(pos))) {
+            this.set(pos, obj);
+        }
+    }
+
+    move(fromPos, toPos, condition) {
+        if (condition(this.get(fromPos), this.get(toPos))) {
+            this.set(toPos, this.get(fromPos));
+            this.set(fromPos, null);
+        }
+    }
+
+    swap(fromPos, toPos, condition) {
+        if (condition(this.get(fromPos), this.get(toPos))) {
+            var temp = this.get(toPos);
+            this.set(toPos, this.get(fromPos));
+            this.set(fromPos, temp);
+        }
+    }
+
+    setRow(row, objs, condition) {
+        for (var i = 0; i < this.columns; i++) {
+            if (condition(this.get(new Vector2(i, row)))) {
+                this.set(new Vector2(i, row), objs[i % objs.length]);
+            }
+        }
+    }
+
+    setColumn(column, objs, condition) {
+        for (var i = 0; i < this.rows; i++) {
+            if (condition(this.get(new Vector2(column, i)))) {
+                this.set(new Vector2(column, i), objs[i % objs.length]);
+            }
+        }
     }
 }
